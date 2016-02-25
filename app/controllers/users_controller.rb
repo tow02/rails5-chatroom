bf4b1d61a45ce7688c456449bc8530a4301398c9
Paween Theanvanichpant <paween.the@gmail.com>
2016-02-25 16:15:49 +0700

    Delete users/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,11 +23,6 @@
 #
 
 class UsersController < ApplicationController
-
-  def index
-    @users = User.all
-  end
-
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'rooms#show'
   devise_for :users
-  resources :users, only: [:index, :show]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
